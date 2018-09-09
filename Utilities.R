0a2eccb9242a29d983a15dddac4679157d367fee
Mike Silva <mike.a.silva@gmail.com>
2018-09-09 18:51:34 -0400

    Updated Utilities.R
    Changed function to execute SQL instead of returning it

--- Utilities.R
+++ Utilities.R
@@ -1,30 +1,39 @@
 ## Utilities.R
 
-# This file holds functions that are generally helpful
-
-# Read in a SQL file and return a string
-# Credit to: https://stackoverflow.com/questions/44853322/how-to-read-the-contents-of-an-sql-file-into-an-r-script-to-run-a-query
-getSQL <- function(filepath){
-  con = file(filepath, "r")
-  sql.string <- ""
-  
+#' Run an SQL file query by query
+#'
+#' @param db_connection Database connection
+#' @param file_path The path to the SQL file
+#'
+run_sql <- function(db_connection, file_path){
+  # Open a connection to the SQL file
+  con <- file(file_path, "r")
+  # Initialize the SQL string
+  sql_string <- ""
   while (TRUE){
+    # Loop through the SQL file line by line
     line <- readLines(con, n = 1)
-    
-    if ( length(line) == 0 ){
+    if (length(line) == 0){
+      # Break out of the loop
       break
     }
-    
-    line <- gsub("\\t", " ", line)
-    
-    if(grepl("--",line) == TRUE){
-      line <- paste(sub("--","/*",line),"*/")
+    # Check for comments
+    if (grepl( "--", line) == TRUE){
+      # Replace them with the /* */ type comments
+      line <- paste(gsub("--", "/*", line), "*/")
     }
-    
-    sql.string <- paste(sql.string, line)
+    # Add the line to the end of the SQL string
+    sql_string <- paste(sql_string, line)
+    # Check for the end of a query
+    if (grepl(";", sql_string)){
+      # Found one so execute the SQL string
+      results <- dbSendQuery(db_connection, sql_string)
+      # Clear the result set
+      dbClearResult(results)
+      # Reset the SQL string
+      sql_string <- ""
+    }
   }
-  
+  # Close the connection to the file
   close(con)
-  return(sql.string)
 }
-
