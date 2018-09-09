a6bfc6331287f0f9973dde87855f696fcaeab7fc
Mike Silva <mike.a.silva@gmail.com>
2018-09-09 09:56:47 -0400

    More work on Setup.R

--- Setup.R
+++ Setup.R
@@ -1,9 +1,17 @@
 ## Setup.R
 
+# Load the needed libraries
+library(RMySQL)
+
 # Load in the MySQL credentials
 source('MySQL Settings.R')
 
-# Load in 
+# Load in the Utility functions
 source('Utilities.R')
 
-setup_db_sql <- getSQL('Setup Database.sql')
+# Connect to the database
+conn <- dbConnect(RMySQL::MySQL(), user = user, password = password, host = host)
+
+#  Read in the SQL to set up the database
+setup_db_sql <- getSQL('Setup Database.sql') # Function found in Utilities.R
+dbSendQuery(conn, setup_db_sql)
