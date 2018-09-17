b6709197f175065692e0f0008874daafa394ba12
Mike Silva <mike.a.silva@gmail.com>
2018-09-17 13:48:10 -0400

    More Work of Project
    Extended the populating of the locations table in the database

--- Setup.R
+++ Setup.R
@@ -1,7 +1,23 @@
 ## Setup.R
 
-# Load the needed libraries
-library("DBI")
+# Set your Census API Key
+# If you don't have a key get one at https://api.census.gov/data/key_signup.html
+Sys.setenv(CENSUS_KEY = "YOUR API KEY HERE")
+Sys.setenv(CENSUS_KEY = "b7ce43a2a321d4efab9598b42e588bd9f50d3881")
+
+# The minimum number of people in the 2010 census to be in the study's universe
+location_pop_threshold = 1000000
+
+# Loop through the needed libraries
+libraries <- c("DBI", "RMySQL", "censusapi", "dplyr")
+for (l in libraries){
+  # If the library is not installed, install it
+  if(!is.element(l, .packages(all.available = TRUE))){
+    install.packages(l)
+  }
+  # Load the library
+  library(l, character.only = TRUE)
+}
 
 # Load in the MySQL credentials
 source("MySQL Settings.R")
@@ -10,11 +26,24 @@ source("MySQL Settings.R")
 source("Utilities.R")
 
 # Connect to the database
-driver <- RMySQL::MySQL()
-conn <- dbConnect(driver, user = user, password = password, host = host)
+conn <- mysql_connect(user, password, host) # Function found in Utilities.R
 
 #  Read in the SQL to set up the database
+message("Setting up the MySQL Database")
 run_sql(conn, "Setup Database.sql") # Function found in Utilities.R
+
+# Disconnect from the database
+disconnected <- dbDisconnect(conn)
+
+# Connect to the database but specify the database name
+conn <- mysql_connect(user, password, host, 'DATA607')
+
+# Create a data.frame of locations we want to collect data for
+source("Get Locations.R")
+
+# Save the locations data.frame to MySQL
+message("Saving locations to MySQL table")
+dbWriteTable(conn, 'LOCATIONS', locations, append = TRUE)
 
 # Disconnect from the database
 disconnected <- dbDisconnect(conn)
--- Utilities.R
+++ Utilities.R
@@ -1,5 +1,29 @@
 ## Utilities.R
 
+
+#' Run an SQL file query by query
+#'
+#' @param full_name The full text string
+#'
+get_location_short_name <- function(full_name){
+  # The full_name follows this format:
+  # city1-city2, state1-state2 Metro Area
+  # We want the short name to be:
+  # city1, state1
+  # Example: Atlanta-Sandy Springs-Roswell, GA Metro Area becomes Atlanta, GA
+  # Split the long name on ", "
+  parts <- unlist(strsplit(full_name, ", "))
+  # Extract the city name
+  part1 <- unlist(strsplit(parts[1], "-"))
+  part1 <- unlist(strsplit(part1[1], "/"))
+  city_name <- part1[1] 
+  # We want the first state listed in the 2nd part
+  state_name <- substr(parts[2],0,2)
+  # Here's the short name
+  short_name <- (paste0(city_name,", ", state_name))
+  short_name
+}
+                    
 #' Run an SQL file query by query
 #'
 #' @param db_connection Database connection
@@ -36,4 +60,11 @@ run_sql <- function(db_connection, file_path){
   }
   # Close the connection to the file
   close(con)
+}
+
+# Connects to a MySQL database
+mysql_connect <- function(user, password, host, dbname = NULL){
+  driver <- RMySQL::MySQL()
+  conn <- dbConnect(driver, dbname = dbname, user = user, password = password, host = host)
+  conn
 }
